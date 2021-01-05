89dd1caba7604ffee09596a7922c05b228d8c00b
raphaelgyo <raphael.granizo@gmail.com>
2021-01-05 11:08:34 +0100

    correction part 1 exo 8

--- Exercice 8/assets/js/result.js
+++ Exercice 8/assets/js/result.js
@@ -3,7 +3,7 @@ validation.addEventListener('click', f_valid);
 function f_valid() {
     //Déclaration de la variable age
     var age = document.getElementById('age').value;
-    // on compare la valeur de la variable age : si elle est supérieure ou égale à 18, alors le visiteur est majeur
+    //on compare la valeur de la variable age : si elle est supérieure ou égale à 18, alors le visiteur est majeur
     if (age >= 18) {
         alert("Vous êtes majeur, vous pouvez accéder à notre site");
     }
